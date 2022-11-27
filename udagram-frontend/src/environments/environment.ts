4943af6b7f1f8a2f3d8c00978a52865354acdf3f
Fivemindz <JD>
2022-11-27 16:16:06 -0600

    updated gitignore file/ added load balancer ip address to frontend url

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a21130fc36038419dbd3ee8f7983b2d5-1500078126.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
