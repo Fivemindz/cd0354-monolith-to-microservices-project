4951a7283fde7c5cb192582cea0cd6ed63c752af
Fivemindz <JD>
2022-11-28 18:47:29 -0600

    added reverse proxy's public ip to frontend

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a62e9a69507344200a810b8d1dccb29d-406432612.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
